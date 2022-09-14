2a5e737e84463e670e3602d4dc14010733858995
astralkizuna <101561811+astralkizuna@users.noreply.github.com>
2022-09-15 00:01:04 +0900

    Update constant.dart

--- lib/dbHelper/constant.dart
+++ lib/dbHelper/constant.dart
@@ -2,6 +2,6 @@
 
         // user = "public01";
         // pass = "pspspsps";
-const MONGO_CONN_URL = "mongodb+srv://public01:pspspsps@cluster01.ad0sso9.mongodb.net/testData01?retryWrites=true&w=majority";
+const MONGO_CONN_URL = "paste apa yg aku share ok tenkiu";
 const USER_COLLECTION = "posts";
 
